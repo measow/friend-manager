432096bb6192761ff122889da9dc2b128386e4a9
Mathew Easow <measow@nvisia.com>
2017-12-30 20:52:02 -0600

    initial commit

---
+++ src/main/java/com/ocupe/FriendManagerApplication.java
@@ -0,0 +1,12 @@
+package com.ocupe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ocupe/FriendManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ocupe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FriendManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
